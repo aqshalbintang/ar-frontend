9ed0c79de56c9c6832b594f674dc734009a2c616
Aqshal <aqshalbintangkrwn@gmail.com>
2025-03-25 13:38:21 +0700

    please

--- scriptCamera.js
+++ scriptCamera.js
@@ -1,24 +1,13 @@
 const apiUrl = "https://ar-backend-production.up.railway.app";
 
-document.addEventListener("DOMContentLoaded", async function () {
+document.addEventListener("DOMContentLoaded", async function() {
     const token = localStorage.getItem("token");
 
     if (!token) {
         alert("Silahkan registrasi atau login dahulu");
         window.location.href = "/";
-        return;
     }
 
-    try {
-        // Minta akses kamera hanya sekali
-        await navigator.mediaDevices.getUserMedia({ video: true });
-        console.log("Akses kamera diberikan.");
-    } catch (error) {
-        console.error("Izin kamera ditolak:", error);
-        return;
-    }
-
-    loadMarkers();
 });
 
 async function loadMarkers() {
@@ -36,8 +25,7 @@ async function loadMarkers() {
                 marker.addEventListener("markerFound", () => {
                     localStorage.setItem("arObjectUrl", target.objectUrl);
                     localStorage.setItem("arType", target.objectUrl.split(".").pop().toLowerCase());
-                    localStorage.setItem("arHasAudio", target.hasAudio ? "true" : "false");
-
+                    
                     window.location.href = "/view.html";
                 });
 
@@ -49,6 +37,10 @@ async function loadMarkers() {
         console.error("Gagal mengambil data marker:", error);
     }
 }
+
+document.addEventListener("DOMContentLoaded", async function () {
+    await loadMarkers();
+});
 
 document.getElementById("backButton").addEventListener("click", function () {
     window.location.href = "/dashboard.html";
